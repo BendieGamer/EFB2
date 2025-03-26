663e3b7deaeda36549d80196505b0f9f3eec0c03
BendieGames <bendiereal@gmail.com>
2025-03-26 17:01:00 -0400

    Update Sounds.js

--- blocks/Sounds.js
+++ blocks/Sounds.js
@@ -1,36 +1,19 @@
 const play_sound_block = {
-    init: function () {
-        this.appendDummyInput()
-            .appendField('play sound')
-            .appendField(new Blockly.FieldTextInput('sound.ogg'), 'SOUND');
-        this.setPreviousStatement(true, null);
-        this.setNextStatement(true, null);
-        this.setColour(160);
-        this.setTooltip('Plays a sound');
-        this.setHelpUrl('');
-    }
+  init: function () {
+    this.appendDummyInput()
+      .appendField('play sound')
+      .appendField(new Blockly.FieldTextInput('sound.ogg'), 'SOUND');
+    this.setPreviousStatement(true, null);
+    this.setNextStatement(true, null);
+    this.setTooltip('Plays a sound');
+    this.setHelpUrl('');
+    this.setColour(160);
+  }
 };
-Blockly.defineBlocksWithJsonArray([
-    {
-        "type": "play_sound_block",
-        "message0": "play sound %1",
-        "args0": [
-            {
-                "type": "field_input",
-                "name": "SOUND",
-                "text": "sound.ogg"
-            }
-        ],
-        "previousStatement": null,
-        "nextStatement": null,
-        "colour": 160,
-        "tooltip": "Plays a sound",
-        "helpUrl": ""
-    }
-]);
+Blockly.common.defineBlocks({ play_sound_block: play_sound_block });
 
 javascript.javascriptGenerator.forBlock['play_sound_block'] = function (block) {
-    const soundFile = block.getFieldValue('SOUND');
-    const code = `var audio = new Audio('${soundFile}'); audio.play();\n`;
-    return code;
-};
+  const soundFile = block.getFieldValue('SOUND');
+  const code = `var audio = new Audio('${soundFile}'); audio.play();\n`;
+  return code;
+}
