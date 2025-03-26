cd7df364fe327b942bb1550a6b5284e2821ea9cf
BendieGames <bendiereal@gmail.com>
2025-03-26 16:31:55 -0400

    Update Sounds.js

--- blocks/Sounds.js
+++ blocks/Sounds.js
@@ -1,28 +1,19 @@
-
-const items_item = {
+const play_sound_block = {
     init: function () {
-        this.appendDummyInput('ITEM')
-            .setAlign(Blockly.inputs.Align.RIGHT)
-            .appendField('item')
-            .appendField(new Blockly.FieldMinecraftItemInput(false, true), 'ITEM');
-        this.setInputsInline(true);
-        this.setOutput(true, null);
-        this.setTooltip('Returns an Item instance (for comparisons)');
-        this.setHelpUrl('https://nurmarvin.github.io/Minecraft-1.8-JavaDocs/net/minecraft/item/Item.html');
-        this.setColour(225);
+        this.appendDummyInput()
+            .appendField('play sound')
+            .appendField(new Blockly.FieldTextInput('sound.ogg'), 'SOUND');
+        this.setPreviousStatement(true, null);
+        this.setNextStatement(true, null);
+        this.setColour(160);
+        this.setTooltip('Plays a sound');
+        this.setHelpUrl('');
     }
 };
-Blockly.common.defineBlocks({ items_item: items_item });
-javascript.javascriptGenerator.forBlock['items_item'] = function () {
-    const item_primitive = this.getFieldValue('ITEM');
-    const object = {
-        type: item_primitive.split("/")[0],
-        id: item_primitive.split("/")[1].split("@")[0]
-    }
-    if (object.type === "item") {
-        return [`(ModAPI.items["${object.id}"]?.getRef() || null)`, javascript.Order.NONE]
-    } else {
-        return [`ModAPI.util.getItemFromBlock(ModAPI.blocks["${object.id}"]?.getRef() || null)`, javascript.Order.NONE]
-    }
-}
+Blockly.common.defineBlocks({ play_sound_block: play_sound_block });
+javascript.javascriptGenerator.forBlock['play_sound_block'] = function () {
+    const soundFile = this.getFieldValue('SOUND');
+    return `var audio = new Audio('${soundFile}'); audio.play();\n`;
+};
+
 
