5c8c6bd260e4d28fc0be6ed059d5de536bd6ff7b
BendieGames <bendiereal@gmail.com>
2025-03-26 16:44:41 -0400

    Update Sounds.js

--- blocks/Sounds.js
+++ blocks/Sounds.js
@@ -10,11 +10,27 @@ const play_sound_block = {
         this.setHelpUrl('');
     }
 };
-Blockly.common.defineBlocks({ play_sound_block: play_sound_block });
+Blockly.defineBlocksWithJsonArray([
+    {
+        "type": "play_sound_block",
+        "message0": "play sound %1",
+        "args0": [
+            {
+                "type": "field_input",
+                "name": "SOUND",
+                "text": "sound.ogg"
+            }
+        ],
+        "previousStatement": null,
+        "nextStatement": null,
+        "colour": 160,
+        "tooltip": "Plays a sound",
+        "helpUrl": ""
+    }
+]);
 
-javascript.javascriptGenerator.forBlock['play_sound_block'] = function () {
-    const soundFile = this.getFieldValue('SOUND');
+javascript.javascriptGenerator.forBlock['play_sound_block'] = function (block) {
+    const soundFile = block.getFieldValue('SOUND');
     const code = `var audio = new Audio('${soundFile}'); audio.play();\n`;
     return code;
 };
-
