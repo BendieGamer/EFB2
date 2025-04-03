49e663e94a615fd9f04c361b2fbb3bd02e2cd3ae
BendieGames <bendiereal@gmail.com>
2025-04-03 13:44:41 -0400

    Update SoundsUploader.js

--- primitives/SoundsUploader.js
+++ primitives/SoundsUploader.js
@@ -17,16 +17,14 @@ PRIMITIVES["SoundUploader"] = {
             ModAPI.addEventListener("lib:asyncsink", async () => {
                 const filePath = \`resourcepacks/AsyncSinkLib/assets/minecraft/sounds/\`${soundName}`.ogg\`;
                 
-                // Set the sound file
                 AsyncSink.setFile(filePath, await (await fetch(\`data:audio/ogg;base64,\`${audioBase64}`\`)).arrayBuffer());
                 
-                // Register the sound
                 AsyncSink.Audio.register(\`sounds.\`${soundName}`\`, AsyncSink.Audio.Category.ANIMALS, [
                     {
                         path: \`sounds/\`${soundName}`.ogg\`,
                         pitch: 1,
                         volume: 1,
-                        streaming: false // use for large audio files
+                        streaming: false
                     }
                 ]);
             });
